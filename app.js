e5dd7a27f2bfad6c3d3701b151eb508318a0f2b0
MrMattsmind <tips.dfmedia@gmail.com>
2021-05-23 19:18:01 -0400

    More refactoring, and playing with EJS

--- app.js
+++ app.js
@@ -2,16 +2,21 @@
 const express = require('express');
 const path = require('path');
 
-const port = 3000;
-
 const app = express();
+const port = 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
 
-    res.render('home')
+    const welcomeTitle = "Welcome to my ToDo List";
+    const timeNow = new Date();
+
+    res.render('home', {
+        welcomeTitle: welcomeTitle,
+        timeNow: timeNow,
+    });
 });
 
 
