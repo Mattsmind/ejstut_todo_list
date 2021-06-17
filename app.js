de49fe98e83d9c148d4396346e28cd2524f55fa3
MrMattsmind <tips.dfmedia@gmail.com>
2021-06-17 00:29:00 -0400

    Laid out basic structure of the web app. Added the form. Added JQuery and Bootstrap.

--- app.js
+++ app.js
@@ -8,17 +8,30 @@ const port = 3000;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
 
-    const welcomeTitle = "Welcome to my ToDo List";
-    const timeNow = new Date();
+    const today = new Date();
+
+    var options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+    };
+
+    var day = today.toLocaleDateString('en-US', options);
 
     res.render('list', {
-        welcomeTitle: welcomeTitle,
-        timeNow: timeNow,
+        currentDate: day,
     });
 });
 
+app.post('/', (req, res) => {
+    console.log(req.body.newItem);
+
+    res.send("<h1>SUCCESS!</h1>");
+});
 
 app.listen(port, () => {
     console.log('Todo app up and running on ' + port + '.');
