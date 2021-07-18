1c3bf681a75856def59b19debc28b0ce196c731b
MrMattsmind <tips.dfmedia@gmail.com>
2021-07-18 19:57:36 -0400

    Hooked into MongoDB using mongoose. Added CRUD functions. Custom Lists.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 const date = require(path.join(__dirname + '/modules', 'date.js'));
 
 const app = express();
@@ -13,38 +14,141 @@ app.use('/static', express.static(path.join(__dirname, 'static')));
 
 app.use(express.urlencoded({extended: true}));
 
-const items = [];
-const workItems = [];
+const uri = 'mongodb://localhost:27017/todolistDB';
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+const itemsSchema = {
+    name: String
+};
+
+const Item = mongoose.model('Item', itemsSchema);
+
+/*-- DEFAULT ITEMS ------------------------------------------------------------------------------------------*/
+
+const item1 = new Item({
+    name: 'Welcome to the todo List!'
+});
+
+const item2 = new Item({
+    name: 'Fill in the field below and hit the "Add Item" button.'
+});
+
+const item3 = new Item({
+    name: '<---- Click this to delete an item.'
+});
+
+const defaultItems = [item1, item2, item3];
+
+/*-- END DEFAULT ITEMS --------------------------------------------------------------------------------------*/
+
+const listSchema = {
+    name: String,
+    items: [itemsSchema]
+};
+
+const List = mongoose.model('List', listSchema);
+
 
 app.get('/', (req, res) => {
 
     const day = date.getDate();
 
-    res.render('list', {
-        list: '/',
-        listTitle: day,
-        items: items,
-    });
-});
+    Item.find({}, (err, items) => {
 
-app.get('/work', (req, res) => {
-    res.render('list', {
-        list: '/work',
-        listTitle: 'Work List',
-        items: workItems,
+        if (err) {
+            console.log('DATABSE LOOKUP ERROR::: %s', err);
+        } else if (items.length === 0) {
+            Item.insertMany(defaultItems, (err) => {
+                if (err) {
+                    console.log('INSERT ERROR::: %s', err);                     
+                } else {
+                    console.log('Database populated with default items.');      
+                    res.redirect('/');
+                }
+            });
+        } else {
+            res.render('list', {
+                listTitle: day,
+                items: items,
+            });
+        }
     });
 });
 
 app.post('/', (req, res) => {
-    items.push(req.body.newItem);
-    res.redirect('/');
+
+    const itemName = req.body.newItem;
+    const listName = req.body.list;
+
+    const newItem = new Item({
+        name: itemName
+    });
+
+    if (listName === date.getDate()) {
+        newItem.save();
+        res.redirect('/');
+    } else {
+        List.findOne({ name: listName }, (err, result) => {
+            if (!err) {
+                result.items.push(newItem);
+                result.save();
+                res.redirect('/' + listName);
+            } else {
+                console.log(err);
+            }
+        });
+    }
 });
 
-app.post('/work', (req, res) => {
-    workItems.push(req.body.newItem);
-    res.redirect('/work');
-}); 
+app.post('/delete', (req, res) => {
+
+    const itemId = req.body.checkbox;
+
+    Item.findByIdAndRemove( itemId, err => {
+        if (err) {
+            console.log('DELETION ERROR::: %s', err);
+        } else {
+            console.log('Deleted Entry: _id::: %s', itemId);
+            res.redirect('/');
+        }
+    });
+});
+
+app.get('/:listTitle', (req, res) => {
+    const listTitle = req.params.listTitle;
+
+    List.findOne({ name: listTitle }, (err, result) => {
+        if (!err) {
+            if (!result) {
+                //create new list
+                
+                const list = new List({
+                    name: listTitle,
+                    items: defaultItems
+                });
+
+                list.save();
+                
+                res.redirect('/' + listTitle);
+
+            } else {
+                //show list
+                res.render('list', {
+                    listTitle: result.name,
+                    items: result.items
+                })
+            }
+        } else {
+            console.log(err);
+        }
+    });
+});
 
 app.listen(port, () => {
-    console.log('Todo app up and running on ' + port + '.');
+    console.log('Todo app up and running on port %s.', port);
 });
